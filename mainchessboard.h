0c8e669fb3780b6ea2aa8242e851844172b9b991
Luis <luis.ribeiro1996@hotmail.com>
2024-03-23 13:18:17 +0000

    Pull problems fixed!

--- mainchessboard.h
+++ mainchessboard.h
@@ -2,6 +2,9 @@
 #define MAINCHESSBOARD_H
 
 #include <QMainWindow>
+#include <QTimer>
+#include <QMediaPlayer>
+#include <QAudioOutput>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +20,28 @@ public:
     MainChessBoard(QWidget *parent = nullptr);
     ~MainChessBoard();
 
+signals:
+    void timeout();
+    void windowShown();
+    void windowClosed();
+    void clicked();
+
+public slots:
+    void startTimer();
+    void stopTimer();
+    void updateTime();
+    void showEvent(QShowEvent *event) override;
+    void closeEvent(QCloseEvent *event) override;
+    void playNewGame();
+    void quitGame();
+    void cleanup();
+
 private:
     Ui::MainChessBoard *ui;
+    QTimer *timer; //declaration of timer as member variable
+    int seconds = 0;
+    QMediaPlayer *player;
+    QAudioOutput *audioOutput;
+
 };
 #endif // MAINCHESSBOARD_H
